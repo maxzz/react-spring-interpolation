b2a762e91894e87597631780aabb4b1fedd9d1ac
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-14 17:37:59 -0700

    Bug: Spring definition is missing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import MainDemo from "./components/MainDemo";
 
 function App() {
   return (
     <div className="App">
-        aa
+        <MainDemo />
     </div>
   );
 }
