283a043d701384b3b76dd7755318ca5ca5e61163
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-18 20:02:12 -0700

    Feature: render props experiments 4 render anim props

--- src/components/Demo3DRotation.tsx
+++ src/components/Demo3DRotation.tsx
@@ -3,14 +3,22 @@ import { Spring, config, animated } from 'react-spring/renderprops'
 
 function Demo3DRotation() {
     const [visible, setVisible] = React.useState(false);
-    
+    const [showProps, setShowProps] = React.useState(true);
+
     return (
-        <div className="m-4" style={{transformStyle: 'preserve-3d', perspective: '100px'}}>
-            <button className="px-2 py-1 border rounded text-gray-100 hover:bg-gray:500" onClick={() => setVisible(!visible)}>{`${visible ? 'Hide' : 'Show'} render props interpolations`}</button>
-            {visible && <div className="pt-4 ml-20">
+        <div className="m-4" style={{ transformStyle: 'preserve-3d', perspective: '50px' }}>
+            <button
+                className="px-2 py-1 border rounded text-gray-100 hover:bg-gray-500 "
+                onClick={() => setVisible(!visible)}>{`${visible ? 'Hide' : 'Show'} render props interpolations`}
+            </button>
+            <button
+                className="ml-2 px-2 py-1 border rounded text-gray-100 hover:bg-gray-500 "
+                onClick={() => setShowProps(!showProps)}>{`${showProps ? 'Hide' : 'Show'} anim props`}
+            </button>
+            {visible && <div className="pt-4 flex justify-center relative">
                 <Spring
                     from={{
-                        width: 300,
+                        width: 100,
                         padding: 0,
                         background: 'linear-gradient(to right, #30e8bf, #ff8235)',
                         transform: 'translate3d(100px,0,0) scale(2) rotateX(90deg)',
@@ -22,12 +30,12 @@ function Demo3DRotation() {
                         number: 2000,
                     }}
                     to={{
-                        width: 200, //'auto'
+                        width: 500, //'auto'
                         padding: 20,
                         background: 'linear-gradient(to right, #009fff, #ec2f4b)',
                         transform: 'translate3d(0px,0,0) scale(1) rotateX(0deg)',
                         boxShadow: '0px 1px 2px 0px rgba(0,0,0,0.4)',
-                        borderBottom: '1px solid #e5e7eb',
+                        borderBottom: '0px solid #e5e7eb',
                         shape: 'M20,20 L20,380 L380,380 L380,20 L20,20 Z',
                         textShadow: '0px 5px 15px rgba(255,255,255,0.5)',
                         color: 'yellow',
@@ -36,27 +44,34 @@ function Demo3DRotation() {
                     config={(key: string) => {
                         switch (key) {
                             //case 'color': return {...config.slow, delay: 400};
-                            case 'color': return {duration: 2000};
-                            case 'number': return {delay: 1000, duration: 2000};
-                            case 'boxShadow': return {duration: 2000};
-                            case 'borderBottom': return {delay: 2000};
+                            case 'color': return { duration: 2000 };
+                            case 'number': return { delay: 1000, duration: 2000 };
+                            case 'boxShadow': return { duration: 2000 };
+                            case 'borderBottom': return { delay: 2000 };
                             default: return config.default;
                         }
                     }}
                 >
-                    {props =>
-                        <animated.div
-                            style={{
-                                boxShadow: props.boxShadow,
-                                width: props.width,
-                                borderBottom: props.borderBottom,
-                                color: props.color,
-                                transform: props.transform,
-                            }}
-                            className="w-10 h-10 border rounded-md bg-red-400 flex items-center justify-center"
-                        >
-                            <animated.span className="c">Lets go {props.number.toFixed(0)}</animated.span>
-                        </animated.div>
+                    {props => {
+                        //console.log(props);
+                        return (<>
+                            <animated.div
+                                style={{
+                                    boxShadow: props.boxShadow,
+                                    width: props.width,
+                                    borderBottom: props.borderBottom,
+                                    color: props.color,
+                                    transform: props.transform,
+                                }}
+                                className="h-40 bg-red-800 flex items-center justify-center"
+                            >
+                                <animated.span className="c"> {`${!props.number ? 'Ready' : ''}`} {props.number ? props.number.toFixed(0) : ''}</animated.span>
+                                {showProps && props.number &&<div className="absolute top-0 left-0 text-green-300">
+                                    <animated.pre className="pointer-events-none" style={{ fontSize: '8px' }}>{JSON.stringify(props, null, 4)}</animated.pre>
+                                </div>}
+                            </animated.div>
+                        </>)
+                    }
                     }
                 </Spring>
             </div>}
